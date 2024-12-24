77084521c562cd771a425d7756568a7c202abfb5
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-12-24 09:55:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Candidate c = new Candidate();
+
+            //c.GetCandidateDetails();
+            //c.DisplayCandidateDetails();
+
+
+
+            //Staff[] s = new Staff[5];
+
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    s[i] = new Staff();
+            //    s[i].GetDetails();
+            //}
+
+            //Console.WriteLine("\nName & Salary Of HOD's :-> ");
+
+            //for (int i = 0; i < s.Length; i++) 
+            //{ 
+            //    if (s[i].Designation == "HOD")
+            //    {
+            //        s[i].DisplayDetails();
+            //    }
+            //}
+
+
+            //Bank_Account account = new Bank_Account();
+
+            //account.GetAccountDetails();
+            //account.DisplayAccountDetails();
+
+
+
+            //Student student = new Student();
+
+            //student.DisplayStudentDetails();
+
+
+
+            //Rectangle r = new Rectangle(2, 5);
+
+
+
+
+            //Interest account = new Interest();
+
+            //account.GetAccountDetails();
+
+            //account.GetInterest();
+
+
+
+            //Console.Write("Enter Basic Salary : ");
+            //float Basic = float.Parse(Console.ReadLine());
+
+            //Console.Write("Enter TA Salary : ");
+            //float TA = float.Parse(Console.ReadLine());
+
+            //Salary employeeSalary = new Salary(Basic, TA);
+
+            //employeeSalary.DisplaySalaryDetails();
+
+
+
+
+            //Distance d = new Distance(2,5);
+
+            //d.CalculateDistance();
+
+
+
+            //Table t = new Table("Wood",299.99,1.2,2.5);
+
+            //t.DisplayTableDetails();
+
+
+
+
+            Employee e = new Employee("Nikunj Rathod");
+
+            e.Basic_sal(30000);
+
+
+            Salary1 s = new Salary1(30000,5000,2000,3000);
+
+            s.Disp_sal();
+            s.Gross_sal();
+        }
+    }
+}
--- Student.cs
+++ Student.cs
@@ -4,42 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab_2
+namespace LAB_2
 {
     internal class Student
     {
-        public int Enrollment_no;
-        public String Student_Name;
+        public long Enrollment_No;
+        public string Student_Name;
         public int Semester;
         public float CPI;
         public float SPI;
 
-
-        public void GetStudentDetails()
+        public Student()
         {
-            Console.WriteLine("enter Enrollment no:");
-            Enrollment_no = int.Parse(Console.ReadLine());
+            Console.Write("Enter Enrollment No. : ");
+            Enrollment_No = long.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter Student_Name:");
+            Console.Write("Enter Student Name : ");
             Student_Name = Console.ReadLine();
 
-            Console.WriteLine("enter Semester:");
+            Console.Write("Enter Semester : ");
             Semester = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter CPI");
+            Console.Write("Enter CPI : ");
             CPI = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter SPI");
+            Console.Write("Enter SPI : ");
             SPI = float.Parse(Console.ReadLine());
         }
 
         public void DisplayStudentDetails()
         {
-            Console.WriteLine("student:"+Enrollment_no);
-            Console.WriteLine("student:" + Student_Name);
-            Console.WriteLine("student:"+Semester); 
-            Console.WriteLine("student:"+CPI);
-            Console.WriteLine("student:"+SPI);
+            Console.WriteLine("\n\nStudent Details :-> ");
+
+            Console.WriteLine("\nEnrollment No. : " + Enrollment_No);
+            Console.WriteLine("Name : " + Student_Name);
+            Console.WriteLine("Semester : " + Semester);
+            Console.WriteLine("CPI : " + CPI);
+            Console.WriteLine("SPI : " + SPI);
         }
     }
 }
