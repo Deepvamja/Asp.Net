62dfa76f5d63e6647fbaa5c814093cdc586efe0a
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-12-26 19:12:15 +0530

    Add files via upload

---
+++ Distance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2
+{
+    internal class Distance
+    {
+        public int dist1;
+        public int dist2;
+        public int dist3;
+
+        public Distance(int dist1, int dist2)
+        {
+            this.dist1 = dist1;
+            this.dist2 = dist2;
+        }
+
+        public void CalculateDistance()
+        {
+            dist3 = dist1 + dist2;
+
+            Console.WriteLine("Sum : " + dist3);
+        }
+    }
+}
---
+++ Gross.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2
+{
+    internal interface Gross
+    {
+        void Gross_sal();
+    }
+
+    internal class Salary1 : Gross
+    {
+        public double HRA;
+        public double TA;
+        public double DA;
+        public double Basic;
+
+        public Salary1(double basic, double HRA, double TA, double DA)
+        {
+            this.Basic = basic;
+            this.HRA = HRA;
+            this.TA = TA;
+            this.DA = DA;
+        }
+        public void Gross_sal() 
+        {
+            double grossSalary = Basic + HRA + TA + DA;
+            Console.WriteLine($"Gross Salary: {grossSalary}");
+        }
+
+        public void Disp_sal()
+        {
+            Console.WriteLine($"Basic Salary: {Basic}");
+            Console.WriteLine($"HRA: {HRA}");
+            Console.WriteLine($"TA: {TA}");
+            Console.WriteLine($"DA: {DA}");
+        }
+    }
+
+    internal class Employee
+    {
+        public string Name;
+
+        public Employee(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Basic_sal(double basic)
+        {
+            Console.WriteLine($"Basic Salary for {Name}: {basic}");
+        }
+    }
+}
