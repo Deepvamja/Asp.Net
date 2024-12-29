1e702d1774598643c67108efe34e7bcc8bb220d0
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-12-29 06:22:57 +0530

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    internal interface Calculate
+    {
+        int Addition(int a, int b);
+        int Subtraction(int a, int b);
+    }
+
+    internal class Result : Calculate
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b) 
+        { 
+            return a - b;
+        }
+    }
+}
