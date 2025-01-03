b42f1b13f4d9ddf7fdbf898b9bee3906b48fed2d
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-03 09:07:08 +0530

    Add files via upload

---
+++ AddofTwoNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class AddofTwoNum
+    {
+        public int Addition(int a, int b)
+        {
+            return(a + b);
+
+        }
+        public float Addition(float a, float b)
+        {
+            return (a + b);
+        }
+    }
+}
---
+++ Hospital.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class Hospital
+    {
+        public virtual void HospitalDetails(string Name,string City)
+        {
+            Console.WriteLine("HospitalName:" + Name + "\nHospitalCity:" + City);
+
+        }
+    }
+    internal class Apollo:Hospital
+    {
+        public override void HospitalDetails(string Name, string City)
+        {
+            base.HospitalDetails(Name, City);
+
+        }
+    }
+    internal class Wockhardt : Hospital
+    {
+        public override void HospitalDetails(string Name, string City)
+        {
+            base.HospitalDetails(Name,City);
+
+        }
+    }
+    internal class Gokul_Superspeciality: Hospital
+    {
+        public override void HospitalDetails(string Name, string City)
+        {
+            base.HospitalDetails(Name, City);
+
+        }
+    }
+}
