90afe60f7628b458fd4df1d81804050318e3d1b2
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-07 17:52:44 +0530

    Add files via upload

---
+++ lab_4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PROGRAM 1
+
+            //  AddofTwoNum aa = new AddofTwoNum();
+
+            //Console.WriteLine(aa.Addition(2,3));
+            //Console.WriteLine(aa.Addition(2.3f,5.3f));
+
+            //program 2
+
+            //area a = new area();
+            //Console.WriteLine(a.areaof(2, 2));
+            //Console.WriteLine(a.areaof(2, 3));
+
+            //PROGRAM 3
+
+            //RBI R = new RBI();
+            //Console.WriteLine(R.CalculateInterest(10000, 2.5));
+
+            //HDFC H = new HDFC();
+            //Console.WriteLine(H.CalculateInterest(12000, 6.7));
+
+            //SBI S = new SBI();
+            //Console.WriteLine(S.CalculateInterest(15000, 4.5));
+
+            //ICIC IC = new ICIC();
+            //Console.WriteLine(IC.CalculateInterest(13000, 6.7));  
+
+            // PROGRAM 4
+
+
+            //Apollo AA = new Apollo();
+            //AA.HospitalDetails("HARSH", "RAJKOT");
+
+
+            //Wockhardt WW = new Wockhardt();
+            //AA.HospitalDetails("NIKUNG", "AHMEDABAD");
+
+            //Gokul_Superspeciality GG = new Gokul_Superspeciality();
+            //GG.HospitalDetails("JAY", "SURAT");
+
+
+            // PROGRAM 5
+
+            Shape ss = new Shape();
+            Console.WriteLine(ss.area(2, 2));
+            Console.WriteLine(ss.area(2, 3));
+            Console.WriteLine(ss.area(2.2));
+
+
+
+
+        }
+    }
+}
---
+++ lab_4/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4
+{
+    internal class Shape
+    {
+        public int area(int x)
+        {
+            return (x * x);
+        }
+        public int area(int x,int y)
+        {
+            return (x * y);
+        }
+        public double area(double a)
+        {
+            return Math.PI *a *a;
+        }
+    }
+}
