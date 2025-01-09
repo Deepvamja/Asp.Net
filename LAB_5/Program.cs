0921d026c2421c81082afa21ac370ef621a36722
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-09 08:51:33 +0530

    Add files via upload

---
+++ LAB_5/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_5
+{
+    internal class Program
+    {
+        private static object st;
+
+        static void Main(string[] args)
+
+
+
+
+
+        {
+            // program 1
+
+            //ArrayList StudentName = new ArrayList();
+            //StudentName.Add("deep");
+            //StudentName.Add("nikun");
+            //StudentName.Add("harsh");
+            //StudentName.Add("NILAY");
+            //StudentName.Add("KEVIN");
+
+
+            //StudentName.Remove("nikun");
+            //StudentName.RemoveRange(1, 2);
+            //StudentName.Clear();
+            //foreach (object student in StudentName)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+
+            // PROGRAM 2
+
+            //List<string> StudentName = new List<string>();
+            //StudentName.Add("viven");
+            //StudentName.Add("jetan");
+            //StudentName.Add("harsh");
+            //StudentName.Add("paten");
+
+
+            //StudentName.Remove("harsh");
+            //StudentName.RemoveRange(1, 2);
+            //StudentName.Clear();
+            //foreach (object Student  in StudentName)
+            //{
+            //    Console.WriteLine(Student);
+            //}
+
+            // program 3
+            //Stack st = new Stack();
+            //st.Push(1);
+            //st.Push(2); 
+            //st.Push(3);
+            //st.Push(4);
+            //st.Pop();
+            //st.Peek();
+            //Console.WriteLine(st.Contains(1));
+            ////st.Clear();
+
+            //foreach (int Student in st)
+            //{
+            //    Console.WriteLine(Student);
+            //}
+
+            // program 4
+            //Queue<string> que = new Queue<string>();
+
+            //que.Enqueue("a");
+            //que.Enqueue("b");
+            //que.Enqueue("c");
+            //que.Enqueue("d");
+            //Console.WriteLine(que.Dequeue());
+            //que.Peek();
+            //Console.WriteLine(que.Contains("a"));
+            //Console.WriteLine(que.Contains("b"));
+            ////que.Clear();
+
+            //foreach (object item in que)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // program 5
+            //Dictionary<int,string> Dic = new Dictionary<int,string>();
+            //Dic.Add(1, "deep");
+            //Dic.Add(2, "harsh");
+            //Dic.Add(3, "vivek");
+            //Dic.Add(4, "prak");
+            //Dic.Add(5, "nike");
+            //Dic.Remove(1);
+            //Console.WriteLine(Dic.ContainsKey(1));
+            //Console.WriteLine(Dic.ContainsKey(2));
+            //Console.WriteLine(Dic.ContainsValue("deep"));
+            //Dic.Clear();
+
+            //foreach (int i in Dic.Keys)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // program 6
+
+            //Hashtable ht = new Hashtable();
+            //ht.Add(1, "biren");
+            //ht.Add(2, "reddy");
+            //ht.Add("E1", "101");
+            //ht.Add("E2", "103");
+
+            //ht.Remove(1);
+            //Console.WriteLine(ht.ContainsKey(1));
+            //Console.WriteLine(ht.ContainsValue(2));
+
+            //foreach (string key in ht.Keys)
+            //{
+            //    Console.WriteLine(ht[key]);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
