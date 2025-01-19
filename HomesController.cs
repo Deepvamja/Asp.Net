34638c311ec7e7bf69aba55b1767452556d0f5bc
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-19 13:30:24 +0530

    Add files via upload

---
+++ HomesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult employee()
+        {
+            return View();
+        }
+    }
+}
