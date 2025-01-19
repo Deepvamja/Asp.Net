8cbf970ddb24a137c87990e372e5d9d465ddbfe6
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-19 13:31:30 +0530

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
+using WebApplication2.Models;
 
-namespace WebApplication1.Controllers
+namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
