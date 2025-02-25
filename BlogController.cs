79bda4422b5debef4239ea1bfc1c058965a6f4cd
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-02-25 19:44:54 +0530

    Add files via upload

---
+++ BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace estateagency.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
