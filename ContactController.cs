586ee398b41da81a7716335c53d0d0bace1160b1
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-02-26 11:04:49 +0530

    Add files via upload

---
+++ ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace estateagency.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
