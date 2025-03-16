b5f0070621948611702023224229823f68692062
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-03-16 19:09:06 +0530

    Add files via upload

---
+++ FormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
